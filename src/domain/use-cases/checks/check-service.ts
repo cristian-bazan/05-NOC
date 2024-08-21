19a483b0a9342dea93263afd162d9fd3b8720d06
Cristian Bazan <cristian.bazan@yahoo.com>
2024-08-21 16:29:50 -0300

    commit

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -3,9 +3,17 @@ interface CheckServiceUseCase {
     execute(url:string):Promise<boolean>;
 }
 
+type SuccessCallback = () => void;
+type ErrorCallback = (error:string) => void;
+
 export class CheckService implements CheckServiceUseCase{
 
-    async execute(url:string):Promise<boolean>{
+    constructor(
+        private readonly successCallback:SuccessCallback,
+        private readonly errorCallback:ErrorCallback,
+    ) {}
+
+    public async execute(url:string):Promise<boolean>{
 
         try {
 
@@ -14,12 +22,13 @@ export class CheckService implements CheckServiceUseCase{
                 throw new Error(`Error on check service ${url}`);
             }
 
-            console.log(`${url} is ok`);
+            this.successCallback();
             return true;
 
         } catch (error) {
 
             console.log(`${error}`);
+            this.errorCallback(`${error}`);
             return false;
         }
     }
