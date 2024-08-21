61d713e4d388de6f34c6bfad33aa02ef3a240f34
Cristian Bazan <cristian.bazan@yahoo.com>
2024-08-21 15:37:43 -0300

    commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,18 @@
+import {CronService} from "./cron/cron-service";
+import {CheckService} from "../domain/use-cases/checks/check-service";
+
+export class Server{
+
+    public static start(){
+
+        console.log('Server started...');
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                new CheckService().execute('https://google.com');
+            }
+        );
+    }
+
+}
