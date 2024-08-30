e9d94f4d87f9be428fb0a95b11788178fe65f365
Cristian Bazan <cristian.bazan@yahoo.com>
2024-08-29 22:49:46 -0300

    commit

--- src/infrastructure/repositories/log.repository.impl.ts
+++ src/infrastructure/repositories/log.repository.impl.ts
@@ -1,5 +1,18 @@
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 import {LogRepository} from "../../domain/repository/log.repository";
+import {LogDatasource} from "../../domain/datasources/log.datasource";
 
-export class LogRepositoryImpl implements LogRepository{
+export class LogRepositoryImpl implements LogRepository {
+
+    constructor(
+        private readonly logDatasource:LogDatasource,
+    ) {}
+
+    saveLog(log: LogEntity): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        throw new Error("Method not implemented.");
+    }
 
 }
